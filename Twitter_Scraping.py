48b824a3498a4169f33d569dbd43d5423cc0476f
Gowsika S <123367785+Gowsi-1107s@users.noreply.github.com>
2023-04-08 00:49:27 +0530

    Add files via upload

--- Twitter_Scraping.py
+++ Twitter_Scraping.py
@@ -1,106 +1,80 @@
-import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import streamlit as st
+import snscrape.modules.twitter as sntwitter
 import datetime
-import pymongo
 import time
+import pymongo
 
-# REQUIRED VARIABLES
-client = pymongo.MongoClient("mongodb://localhost:27017/")  # To connect to MONGODB
-mydb = client["Twitter_Database"]    # To create a DATABASE
-tweets_df = pd.DataFrame()           # TO CREATE a DATAFRAME
-dfm = pd.DataFrame()
+Client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = Client["Twitter_Scraping"]
 
-# NAVIGATION
+tweets_df = pd.DataFrame()
+df = pd.DataFrame()
 
-nav = st.sidebar.radio('Navigation',["Home", "Twitter Scraping"])   #TO CREATE SIDEEBAR
-if nav == "Home":                              #1ST PAGE
-    st.title(':blue[TWITTER_SCRAPING]:sunglasses:')
-    st.image('https://images.pexels.com/photos/13240228/pexels-photo-13240228.jpeg?auto=compress&cs=tinysrgb&w=600',width=800,output_format="auto")
-    
-if nav == "Twitter Scraping":                  #2ND PAGE
-    option = st.radio('How would you like to search the data in Twitter?',('Keyword', 'Hashtag'))
-    
-    word = st.text_input('Please enter a '+option, 'Example: Data Science')
-    
-    start = st.date_input("Select the start date", datetime.date(2020, 1, 1))
-    
-    end = st.date_input("Select the end date", datetime.date(2023, 4,4))
-    
-    num_tweet = st.slider('Number of tweets to scrap', 0, 1000, 5)
 
+st.header(':blue[TWITTER_SCRAPING]')
+st.image('https://images.pexels.com/photos/13240228/pexels-photo-13240228.jpeg?auto=compress&cs=tinysrgb&w=600',width=400,output_format="auto")
+choice = st.radio(label='How would you like to search the data?',options=['Keyword', 'Hashtag'])
+word = st.text_input('Please enter a '+choice, 'Example: Data Science')
+start_date = st.date_input("Select the start date", datetime.date(2023, 1, 1))
+end_date = st.date_input("Select the end date", datetime.date(2023, 4, 4))
+num_tweet = st.slider('How many tweets to scrape', 0, 1000, 10)
 tweets_list = []
 
-# SCRAPE DATA USING TwitterSearchScraper
+
 if word:
-    if option=='Keyword':
-        for i,tweet in enumerate(sntwitter.TwitterSearchScraper(f'{word} + since:{start} until:{end}').get_items()):
+    if choice == "Keyword":
+        for i,tweet in enumerate(sntwitter.TwitterSearchScraper(f'{word} + since:{start_date} until:{end_date}').get_items()):
             if i>num_tweet:
                 break
-            tweets_list.append([tweet.date, tweet.id, tweet.url, tweet.content, tweet.user.username,tweet.replyCount, tweet.retweetCount,tweet.lang, tweet.source, tweet.likeCount])
-        tweets_df = pd.DataFrame(tweets_list, columns=['Date', 
-                                             'ID', 
-                                             'Url', 
-                                             'Content',
-                                             'Username',
-                                             'Reply count', 
-                                             'Retweet count',
-                                             'Language',
-                                             'Source', 
-                                             'Like count'])
-## SCRAPE DATA USING TwitterHashtagScraper
+            tweets_list.append([tweet.id, tweet.date,  tweet.content, tweet.lang, tweet.user.username, tweet.replyCount, tweet.retweetCount,tweet.likeCount, tweet.source, tweet.url ])
+        tweets_df = pd.DataFrame(tweets_list, columns=['ID','Date','Content', 'Language', 'Username', 'ReplyCount', 'RetweetCount', 'LikeCount','Source', 'Url'])
     else:
-        for i,tweet in enumerate(sntwitter.TwitterHashtagScraper(f'{word} + since:{start} until:{end}').get_items()):
+        for i,tweet in enumerate(sntwitter.TwitterHashtagScraper(f'{word} + since:{start_date} until:{end_date}').get_items()):
             if i>num_tweet:
                 break            
-            tweets_list.append([ tweet.date, tweet.id, tweet.url, tweet.content, tweet.user.username,tweet.replyCount, tweet.retweetCount,tweet.lang, tweet.source, tweet.likeCount])
-        tweets_df = pd.DataFrame(tweets_list, columns=[ 'Date', 
-                                             'ID', 
-                                             'Url', 
-                                             'Content',
-                                             'Username',
-                                             'Reply count', 
-                                             'Retweet count',
-                                             'Language',
-                                             'Source', 
-                                             'Like count'])
+            tweets_list.append([ tweet.id, tweet.date,  tweet.content, tweet.lang, tweet.user.username, tweet.replyCount, tweet.retweetCount,tweet.likeCount, tweet.source, tweet.url ])
+        tweets_df = pd.DataFrame(tweets_list, columns=['ID','Date','Content', 'Language', 'Username', 'ReplyCount', 'RetweetCount', 'LikeCount','Source', 'Url'])
 else:
-    st.warning(option,' cant be empty', icon="⚠️")
-# DOWNLOAD AS CSV
-@st.cache_data # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    st.warning(choice,' cant be empty', icon="⚠️")
+
+@st.cache_data
 def convert_df(df):    
     return df.to_csv().encode('utf-8')
 
-    # DOWNLOAD AS CSV
 if not tweets_df.empty:
-    csv = convert_df(tweets_df)
-    st.download_button(label="Download data as CSV",data=csv,file_name='Twitter_Scraping.csv',mime='text/csv',)
-   
+    col1,col2,col3,col4 = st.columns(4)
+    with col1:
+        csv = convert_df(tweets_df)
+        st.download_button(label="Download data as CSV",data=csv,file_name='Twitter_Scraping.csv',mime='text/csv',)
+        
+    with col2:
+        json_string = tweets_df.to_json(orient ='records')
+        st.download_button(label="Download data as JSON",file_name="Twitter_Scraping.json",mime="application/json",data=json_string,)
+        
+    with col3:
+        if st.button('Upload Data to MongoDB Database'):
+            coll=word
+            coll=coll.replace(' ',' ')
+            mycollection=db[coll]
+            dict=tweets_df.to_dict('records')
+            if dict:
+                mycollection.insert_many(dict) 
+                ts = time.time()
+                mycollection.update_many({}, {"$set": {"KeyWord_or_Hashtag": word+str(ts)}}, upsert=False, array_filters=None)
+                st.success('Successfully uploaded to database', icon="✅")
+                st.balloons()
+            else:
+                st.warning('Cant upload because there are no tweets', icon="⚠️")
+
+if st.button('Show Data'):
+    st.write(tweets_df)
+    st.snow()
+
+with st.expander("click me"):
+    st.write(
+        "Thank you!!!"
+        "Hope you enjoyed Twitter Scraping.")
     
-    # DOWNLOAD AS JSON
-    json_string = tweets_df.to_json(orient ='records')
-    st.download_button(label="Download data as JSON",file_name="Twitter_Scraping.json",mime="application/json",data=json_string,)
 
-    # UPLOAD DATA TO DATABASE
-    if st.button('Upload Tweets to Database'):
-        coll=word
-        coll=coll.replace(' ','_')+'_Tweets'
-        mycoll=mydb[coll]
-        dict=tweets_df.to_dict('records')
-        if dict:
-            mycoll.insert_many(dict) 
-            ts = time.time()
-            mycoll.update_many({}, {"$set": {"KeyWord_or_Hashtag": word+str(ts)}}, upsert=False, array_filters=None)
-            st.success('Successfully uploaded to database', icon="✅")
-            st.balloons()
-        else:
-            st.warning('Cant upload because there are no tweets', icon="⚠️")
-
-    # SHOW TWEETS
-    if st.button('Show Data'):
-        st.write(tweets_df)
-        st.snow()
-
-
-            
 
